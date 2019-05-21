3e173c2ef9e84dfd5b641fc6f0a7d77ba678110d
Zach Overhulser <zoverhulser@gmail.com>
2019-05-21 19:33:11 +0000

    Migrations

--- api/server.js
+++ api/server.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const configureMiddleware = require("../config/middleware");
-/*
 const loginRouter = require("../routes/loginRouter");
 const registrationRouter = require("../routes/registrationRouter");
 const usersRouter = require("../routes/usersRouter");
-*/
 
 // Server initialization
 const server = express();
@@ -12,10 +10,8 @@ const server = express();
 configureMiddleware(server);
 
 //Routes
-/*
 server.use("/api/registration", registrationRouter);
 server.use("/api/login", loginRouter);
 server.use("/api/users", usersRouter);
-*/
 
 module.exports = server;
